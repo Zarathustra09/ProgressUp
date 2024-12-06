6db423e8d4b39d1d44284130d29cf47e79b3e2b8
Zarathustra09 <joshua.pardo30@gmail.com>
2024-12-07 00:45:58 +0800

    Dec 7 2024 - home.blade.php finished. added custom quote generator.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Room;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
@@ -23,6 +26,36 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $parentCount = $this->countParents();
+        $studentCount = $this->countStudent();
+        $roomCount = $this->countRoom();
+
+        // Fetch quotes from the new API
+        $quote = $this->fetchQuotes();
+        $quoteContent = $quote['content'];
+        $quoteAuthor = $quote['author'];
+
+        return view('home', compact('parentCount', 'studentCount', 'roomCount', 'quoteContent', 'quoteAuthor'));
+    }
+
+    public function countParents()
+    {
+        return count(User::where('role_id', '0')->get());
+    }
+
+    public function countStudent()
+    {
+        return count(User::where('role_id', '1')->get());
+    }
+
+    public function countRoom()
+    {
+        return count(Room::all());
+    }
+
+    public function fetchQuotes()
+    {
+        $response = Http::withOptions(['verify' => false])->get('https://api.quotable.io/random');
+        return $response->json();
     }
 }
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
         <a href="#" class="app-brand-link">
-            <img src="#" alt="Progress Up Logo" class="app-brand-logo">
+            <img src="{{asset('logo.png')}}" alt="Progress Up Logo" class="app-brand-logo">
         </a>
 
         <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
