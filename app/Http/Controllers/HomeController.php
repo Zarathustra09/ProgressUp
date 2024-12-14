21ad773831f529282077216d0db360694176a306
Zarathustra09 <joshua.pardo30@gmail.com>
2024-12-14 14:01:23 +0800

    Dec 14 2024 - disabled the API for quotes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -55,7 +55,22 @@ class HomeController extends Controller
 
     public function fetchQuotes()
     {
-        $response = Http::withOptions(['verify' => false])->get('https://api.quotable.io/random');
-        return $response->json();
+        $quotes = [
+            ['content' => 'The only limit to our realization of tomorrow is our doubts of today.', 'author' => 'Franklin D. Roosevelt'],
+            ['content' => 'The purpose of our lives is to be happy.', 'author' => 'Dalai Lama'],
+            ['content' => 'Life is what happens when you’re busy making other plans.', 'author' => 'John Lennon'],
+            ['content' => 'Get busy living or get busy dying.', 'author' => 'Stephen King'],
+            ['content' => 'You have within you right now, everything you need to deal with whatever the world can throw at you.', 'author' => 'Brian Tracy'],
+            ['content' => 'Believe you can and you’re halfway there.', 'author' => 'Theodore Roosevelt'],
+            ['content' => 'The only impossible journey is the one you never begin.', 'author' => 'Tony Robbins'],
+            ['content' => 'Life is short, and it is up to you to make it sweet.', 'author' => 'Sarah Louise Delany'],
+            ['content' => 'The unexamined life is not worth living.', 'author' => 'Socrates'],
+            ['content' => 'Turn your wounds into wisdom.', 'author' => 'Oprah Winfrey'],
+        ];
+
+        // Randomly select a quote
+        $quote = $quotes[array_rand($quotes)];
+
+        return $quote;
     }
 }
